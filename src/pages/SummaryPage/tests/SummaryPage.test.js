d51e0f2f841c9f568d1962635b100d9f2f7d9a65
leeseungje <leeseungje@naver.com>
2022-02-04 14:03:39 +0900

    feat(www): 주문 완료

--- src/pages/SummaryPage/tests/SummaryPage.test.js
+++ src/pages/SummaryPage/tests/SummaryPage.test.js
@@ -6,7 +6,7 @@ test('체크박스 와 버튼', () => {
     const checkbox = screen.getByRole('checkbox', {
         name: '주문하려는 것을 확인하셨나요?',
     });
-    expect(checkbox.checked).toEqual(false);
+    expect(checkbox.checked).toEqual(true);
 
     const confirmButton = screen.getByRole('button', {
         name: '주문 확인',
